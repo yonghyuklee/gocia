d0c61107d4b915004ad68bb32f263d9f93b19f43
Yonghyuk Lee <huckley90@gmail.com>
2026-01-08 16:49:01 +0900

    add relax_steps variable into geomopt_simple

--- gocia/utils/ase.py
+++ gocia/utils/ase.py
@@ -8,7 +8,7 @@ from gocia.geom import get_fragments, del_freeMol, is_bonded, detect_bond_betwee
 from gocia.interface import Interface
 from gocia.geom.frag import *
 
-def geomopt_simple(atoms, my_calc, optimizer='LBFGS', fmax=0.05, label=None, fn_bkup=None):
+def geomopt_simple(atoms, my_calc, optimizer='LBFGS', fmax=0.05, relax_steps=2000, label=None, fn_bkup=None):
     atoms_opt = atoms.copy()
     atoms_opt.calc = my_calc
     write('ini.vasp', atoms)
@@ -37,7 +37,7 @@ def geomopt_simple(atoms, my_calc, optimizer='LBFGS', fmax=0.05, label=None, fn_
         else:
             print(f'Optimizing {atoms_opt.get_chemical_formula()} in {label}')
             
-        for i in range(2000):
+        for i in range(relax_steps):
             dyn.run(fmax=fmax, steps=1)
             if atoms_opt.get_forces().max() > 1000:
                 print(f"Force exceeds threshold.")
